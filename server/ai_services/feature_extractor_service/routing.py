2a0b0621e38bfeac3dea4d117affa0a6528d604a
Mert Ustun <mertustun033@gmail.com>
2025-07-31 23:07:48 +0300

    feat: add feature_extractor service

---
+++ server/ai_services/feature_extractor_service/routing.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, File, Query, UploadFile
+from ai_services.feature_extractor_service.feature_extractor import FeatureExtractor
+
+feature_extractor_router=APIRouter()
+
+@feature_extractor_router.post("/extract-feature")
+async def feature_extractor(language:str=Query(default="english"),
+                            image:UploadFile=File(...),
+                            model:str=Query(default=" ")
+                            ):
+
+    image=await image.read()
+    
+    features= await FeatureExtractor.extract_feature(image=image,
+                                                    language=language,
+                                                    model=model)
+    
+    return features
+    
+
+    
+
+    
