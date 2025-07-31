91ee209dfef84d540bd1e758650f8a24ad9f98b3
Mert Ustun <mertustun033@gmail.com>
2025-07-31 23:08:37 +0300

    feat: add feature extractor and image generation router

--- server/ai_services/main.py
+++ server/ai_services/main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 import uvicorn
 from ai_services.description_service.routing import analyze_image_router
 from ai_services.tag_service.routing import tag_generator_router
+from ai_services.feature_extractor_service.routing import feature_extractor_router
+from ai_services.image_service.routing import image_generation_router
 app=FastAPI()
 
 app.include_router(analyze_image_router)
 app.include_router(tag_generator_router)
+app.include_router(feature_extractor_router)
+app.include_router(image_generation_router)
 @app.get("/")
 def initialize_project():
     return {"message":"BTK-2025 Projesi"}
